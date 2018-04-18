9bc20775da38a733fbc09d24224f6e75f8b0d23f
Nolan McCafferty <njma2016@pomona.edu>
2018-04-18 11:44:13 -0700

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -1,14 +1,22 @@
 import string;
 
+# to remove punctuation
 translator = str.maketrans('', '', string.punctuation)
 
+# initialize lists
 doc = []
 docs = []
 lines = ''
+
+# read text from jobs file
 with open('alljobs.txt', 'r') as rf:
 		for line in rf:
+			
+			# remove punctuation and make lowercase
 			newline = line.translate(translator)
 			newline = newline.lower()
+			
+			# seperate job postings as individual docs
 			if newline == 'zzz\n':
 				doc = lines.split()
 				docs.append(doc)
